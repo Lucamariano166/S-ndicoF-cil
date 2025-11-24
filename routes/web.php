7a9c8f56246f5f3bd5008bc8e033671d10cb654d
Lucamariano166 <lucamariano.lm166@gmail.com>
2025-11-24 13:45:24 -0300

    Disable web middleware to debug 500 error

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/test', function() {
     return 'Laravel is working!';
 });
 
-Route::get('/', [LandingController::class, 'index'])->name('home');
+Route::get('/', [LandingController::class, 'index'])->name('home')->withoutMiddleware(['web']);
 Route::post('/contato', [LandingController::class, 'store'])->name('contato.store');
